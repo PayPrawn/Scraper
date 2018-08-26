590f611511bf914bb7d2abf0bc08924600c03e21
PayPrawn <finlay.scott@rocketmail.com>
2018-08-26 20:19:49 +0100

    fixed errors

--- initialinfect.py
+++ initialinfect.py
@@ -2,6 +2,6 @@ import os
 from time import sleep
 os.chdir("/Users/fin/Desktop/testfolder")
 os.system("git clone https://github.com/PayPrawn/Scraper.git")
-os.chdir("/Scraper/")
+os.chdir("Scraper/")
 #now in correct file with repository cloned
 os.system("python3 ./opentunnel.py")
