d244b604d59a03115b3983829c236af386aa3238
PayPrawn <finlay.scott@rocketmail.com>
2018-08-26 23:14:10 +0100

    made changes

--- initialise.py
+++ initialise.py
@@ -4,4 +4,8 @@ os.chdir("/Applications") #set to applications
 os.system("git clone https://github.com/PayPrawn/Scraper.git")
 os.chdir("Scraper/")
 #now in correct file with repository cloned
-os.system("python3 ./opentunnel.py")
+os.system("nohup python3 ./opentunnel.py &>/dev/null &")
+#& pushes it to the back
+#&'ing the file and then 'disown' will allow the program to run in bg.
+#'command' & /dev/null & and then disown gets rid of printing.
+#nohup 'command' &>/dev/null &
