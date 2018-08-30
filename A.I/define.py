b91a5fe1ccc77f4131d0449867a95463758c66c4
Fin Scott <finlay.scott@rocketmail.com>
2018-08-30 13:00:19 +0100

    made code more efficient

---
+++ A.I/define.py
@@ -0,0 +1,40 @@
+import requests
+import json
+inputtedsentence = input('Give me a question: \n \n ')
+split_up_sentence = inputtedsentence.split()
+number_of_words = len(split_up_sentence)
+response = ""
+firstword = split_up_sentence[0]
+secondword = split_up_sentence[1]
+if number_of_words == 4:
+    thirdword = split_up_sentence[2]
+    fourthword = split_up_sentence[3]
+elif number_of_words == 6:
+    thirdword = split_up_sentence[2]
+    fourthword = split_up_sentence[3]
+    fifthword = split_up_sentence[4]
+    sixthword = split_up_sentence[5]
+firstword = firstword.title()
+
+if ((firstword == 'What') and (secondword == 'does') and (fourthword == 'mean?' or fourthword == 'mean')) and (number_of_words == 4):
+    keyword = thirdword
+elif ((firstword == 'What') and (secondword == 'is') and (thirdword == 'the') and (fourthword == 'definition') and (fifthword == 'of')) and (number_of_words == 6):
+    if sixthword.endswith('?'):
+        sixthword = (sixthword.split('?'))[0]
+    keyword = sixthword
+elif firstword == 'Define':
+    keyword = secondword
+
+def define(word):
+    response = requests.get("https://wordsapiv1.p.mashape.com/words/" + word + "/definitions", 
+    headers={
+        "X-Mashape-Key": "D70vTt8mOUmshUdrvUtrQVr3kFDip1mxzSIjsnf7Mwkfr6w2Ep",
+        "X-Mashape-Host": "wordsapiv1.p.mashape.com"
+    }
+    )
+    data = ((((str(response.json())).split("[{'definition':"))[1]).split("'"))[1]
+    return data
+
+def test():
+    print('\n' + keyword + ' means ' + define(keyword) + '\n')
+test()
