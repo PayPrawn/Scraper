8e785f0c5de0392311e3d56df81f206c0309a3e0
Fin Scott <finlay.scott@rocketmail.com>
2018-09-01 20:52:22 +0100

    added real word finder

--- A.I/Ying/define.py
+++ A.I/Ying/define.py
@@ -2,7 +2,27 @@ import requests
 import numpy as np
 import matplotlib.pyplot as plt
 import json   
-"""
+import urllib.request
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+realwords = []
+def doeswordexist(word):
+    url = ('https://en.oxforddictionaries.com/definition/' + word)
+    r = requests.get(url)
+    data = r.text
+    soup = BeautifulSoup(data, 'html.parser')
+    soup = str(soup.find_all('h3'))
+    if soup.find('[<h3 class="ps pos">') > -1:
+        realwords.append(word)
+def define(word):
+    response = requests.get("https://wordsapiv1.p.mashape.com/words/" + word + "/definitions", 
+    headers={
+        "X-Mashape-Key": "D70vTt8mOUmshUdrvUtrQVr3kFDip1mxzSIjsnf7Mwkfr6w2Ep",
+        "X-Mashape-Host": "wordsapiv1.p.mashape.com"
+    }
+    )
+    data = ((((str(response.json())).split("[{'definition':"))[1]).split("'"))[1]
+    return data
 def definitionprogram():
     inputtedsentence = input('\n Give me a question: \n \n ')
     split_up_sentence = inputtedsentence.split()
@@ -19,7 +39,6 @@ def definitionprogram():
         sixthword = split_up_sentence[5]
     firstword = firstword.title()
 
-
     if ((firstword == 'What') and (secondword == 'does') and (fourthword == 'mean?' or fourthword == 'mean')) and (number_of_words == 4):
         keyword = thirdword
     elif ((firstword == 'What') and (secondword == 'is') and (thirdword == 'the') and (fourthword == 'definition') and (fifthword == 'of')) and (number_of_words == 6):
@@ -30,27 +49,7 @@ def definitionprogram():
             keyword = sixthword
     elif firstword == 'Define':
         keyword = secondword
-"""
-def define(word):
-        response = requests.get("https://wordsapiv1.p.mashape.com/words/" + word + "/definitions", 
-        headers={
-            "X-Mashape-Key": "D70vTt8mOUmshUdrvUtrQVr3kFDip1mxzSIjsnf7Mwkfr6w2Ep",
-            "X-Mashape-Host": "wordsapiv1.p.mashape.com"
-        }
-        )
-        data = ((((str(response.json())).split("[{'definition':"))[1]).split("'"))[1]
-        return data
-
-def doeswordexist(string):
- try:
-    if print(define(string)) != "":
-        return True
- except IndexError:
-   return False
-
-
-
-
+    print('\n' + keyword + ' means ' + define(keyword) + '\n')
 def wordinterpreter():
     print('\n Command list: \n define')
     x = ''
@@ -69,48 +68,35 @@ def creatematrix():
     plt.plot(x, y)
     plt.show()
     plt.title('Personality Matrix')
-
-def define(word):
-        response = requests.get("https://wordsapiv1.p.mashape.com/words/" + word + "/definitions", 
-        headers={
-            "X-Mashape-Key": "D70vTt8mOUmshUdrvUtrQVr3kFDip1mxzSIjsnf7Mwkfr6w2Ep",
-            "X-Mashape-Host": "wordsapiv1.p.mashape.com"
-        }
-        )
-        data = ((((str(response.json())).split("[{'definition':"))[1]).split("'"))[1]
-        return data
-
 def strengthconnection(firstnode, secondnode):
     for i in range(len(connections)):
         if (connections[i][0] == firstnode) and (connections[i][1] == secondnode):
             return connections[i][2]
+def nodes():
+    global connections
+    layer1 = list()
+    layer2 = list()
+    connections = list()
+    for i in range(2):
+        layer2.append(["L1-" + str(i)])
+
+    for i in range(10):
+        layer1.append(["L2-" + str(i)])
+        for n in range(len(layer2)):
+            layer1[i].append(layer2[n][0])
+            connections.append(["L1-"+ str(i),"L2-"+ str(n),0.5])
 
 
-layer1 = list()
-layer2 = list()
-connections = list()
-for i in range(2):
-    layer2.append(["L1-" + str(i)])
-
-for i in range(10):
-    layer1.append(["L2-" + str(i)])
-    for n in range(len(layer2)):
-        layer1[i].append(layer2[n][0])
-        connections.append(["L1-"+ str(i),"L2-"+ str(n),0.5])
+    f = open('connections.txt','w')
+    a = connections
+    f.write(str(a))
+    f.close()
 
 
-f = open('connections.txt','w')
-a = connections
-f.write(str(a))
-f.close()
-
-
-print(layer1)
-print("\n ------------------------------ \n")
-print(layer2)
-print("\n ------------------------------ \n")
-print(connections)
-print("\n ------------------------------ \n")
-print(strengthconnection("L1-9","L2-0"))
-
-def wordanalysis(string):
+    print(layer1)
+    print("\n ------------------------------ \n")
+    print(layer2)
+    print("\n ------------------------------ \n")
+    print(connections)
+    print("\n ------------------------------ \n")
+    print(strengthconnection("L1-9","L2-0"))
