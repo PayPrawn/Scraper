3164f043a3ab494592c5b098cfa2f25231716363
Fin Scott <finlay.scott@rocketmail.com>
2018-09-19 22:02:10 +0100

    added graphing thing

--- NewA.I/probabiltymatrix.py
+++ NewA.I/probabiltymatrix.py
@@ -1,2 +1,16 @@
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
+
+x = []
+start = -100
+for i in range(200):
+    x.append(start)
+    start = start + 1
+y = []
+for i in range(len(x)):
+    y.append(x[i] ** 2)
+
+print(x)
+plt.plot(x, y)
+plt.axis([-50, 50, -50, 50])
+plt.show()
